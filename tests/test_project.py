581034b1772e37894da0b3da76fe4f750daba7ae
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-12-19 11:47:56 -0600

    Add test for relative to absolute path conversion

--- tests/test_project.py
+++ tests/test_project.py
@@ -1,13 +1,16 @@
+import os
+
 import xrdimageutil as xiu
 
-def test_project_instatiation_valid_input_not_null():
+# PROJECT INSTANTIATION
+def test_project_instatiation_with_valid_input_yields_not_null():
     project = xiu.Project(
         project_path=".", 
         mode="databroker"
     )
     assert project is not None
 
-def test_project_instatiation_nonexisting_path_value_error():
+def test_project_instatiation_with_nonexisting_path_yields_value_error():
     try:
         project = xiu.Project(
             project_path="./nonexistingpath", 
@@ -17,7 +20,7 @@ def test_project_instatiation_nonexisting_path_value_error():
     except ValueError:
         assert True
 
-def test_project_instatiation_invalid_path_type_error():
+def test_project_instatiation_with_invalid_path_yields_type_error():
     try:
         project = xiu.Project(
             project_path=47, 
@@ -27,7 +30,7 @@ def test_project_instatiation_invalid_path_type_error():
     except TypeError:
         assert True
 
-def test_project_instatiation_invalid_mode_value_error():
+def test_project_instatiation_with_invalid_mode_yields_value_error():
     try:
         project = xiu.Project(
             project_path=".", 
@@ -37,7 +40,7 @@ def test_project_instatiation_invalid_mode_value_error():
     except ValueError:
         assert True
 
-def test_project_instatiation_invalid_mode_type_error():
+def test_project_instatiation_with_invalid_mode_yields_type_error():
     try:
         project = xiu.Project(
             project_path=".", 
@@ -47,7 +50,11 @@ def test_project_instatiation_invalid_mode_type_error():
     except TypeError:
         assert True
 
-
-
-    
-
+def test_project_instatiation_with_relative_path_yields_absolute_path():
+    relative_path = "."
+    absolute_path = os.path.abspath(relative_path)
+    project = xiu.Project(
+            project_path=".", 
+            mode="databroker"
+        )
+    assert project.path == absolute_path
