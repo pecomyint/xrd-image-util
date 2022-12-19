e001567e6e35340ce661adb75f0e5b5d4b0e0401
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-12-19 11:34:59 -0600

    Add docstring for Project class

--- xrdimageutil/__init__.py
+++ xrdimageutil/__init__.py
@@ -1,7 +1,14 @@
 import os
 
 class Project:
-    """Houses data for a collection of scans."""
+    """Houses data for a collection of scans.
+    
+    :param project_path: A path (relative or absolute) to the project directory
+    :type project_path: str
+    :param mode: Type of project being created. The current acceptable modes 
+        are for (1) the Beamline 6-ID-B SPEC setup and (2) databroker.
+    :type mode: str
+    """
 
     def __init__(
         self, 
@@ -20,5 +27,7 @@ class Project:
             raise TypeError(f"Mode '{mode}' is not an accepted mode.")
         if mode not in ["6IDBspec", "databroker"]:
             raise ValueError(f"Mode '{mode}' is not an accepted mode.")
-            
+
         self.mode = mode
+
+        
