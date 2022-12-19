2e5833a8b18bb8e5500233e19992e98c39ee203b
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-12-19 15:11:28 -0600

    Remove mode

--- tests/test_project.py
+++ tests/test_project.py
@@ -5,56 +5,24 @@ import xrdimageutil as xiu
 # PROJECT INSTANTIATION
 def test_project_instatiation_with_valid_input_yields_not_null():
     project = xiu.Project(
-        project_path=".", 
-        mode="databroker"
+        project_path="."
     )
     assert project is not None
 
 def test_project_instatiation_with_nonexisting_path_yields_value_error():
     try:
         project = xiu.Project(
-            project_path="./nonexistingpath", 
-            mode="databroker"
+            project_path="./nonexistingpath"
         )
         assert False
     except ValueError:
         assert True
 
-def test_project_instatiation_with_invalid_path_yields_type_error():
+def test_project_instatiation_with_nonstring_path_yields_type_error():
     try:
         project = xiu.Project(
-            project_path=47, 
-            mode="databroker"
+            project_path=47
         )
         assert False
     except TypeError:
         assert True
-
-def test_project_instatiation_with_invalid_mode_yields_value_error():
-    try:
-        project = xiu.Project(
-            project_path=".", 
-            mode="caracara"
-        )
-        assert False
-    except ValueError:
-        assert True
-
-def test_project_instatiation_with_invalid_mode_yields_type_error():
-    try:
-        project = xiu.Project(
-            project_path=".", 
-            mode=1234
-        )
-        assert False
-    except TypeError:
-        assert True
-
-def test_project_instatiation_with_relative_path_yields_absolute_path():
-    relative_path = "."
-    absolute_path = os.path.abspath(relative_path)
-    project = xiu.Project(
-            project_path=".", 
-            mode="databroker"
-        )
-    assert project.path == absolute_path
--- xrdimageutil/__init__.py
+++ xrdimageutil/__init__.py
@@ -1,19 +1,15 @@
 import os
 
 class Project:
-    """Houses data for a collection of scans.
+    """Houses data for a collection of databroker catalogs.
     
     :param project_path: A path (relative or absolute) to the project directory
     :type project_path: str
-    :param mode: Type of project being created. The only current acceptable 
-        mode is "databroker".
-    :type mode: str
     """
 
     def __init__(
         self, 
-        project_path, 
-        mode="databroker"
+        project_path
     ) -> None:
         
         if type(project_path) != str:
@@ -23,13 +19,6 @@ class Project:
 
         # Converts path to absolute path
         self.path = os.path.abspath(project_path)
-
-        if type(mode) != str:
-            raise TypeError(f"Mode '{mode}' is not an accepted mode.")
-        if mode not in ["6IDBspec", "databroker"]:
-            raise ValueError(f"Mode '{mode}' is not an accepted mode.")
-
-        self.mode = mode
 
         
         
