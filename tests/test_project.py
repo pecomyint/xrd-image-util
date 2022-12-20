31b1a49f62d2cd7af4e8597dcca6427000c0f36e
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-12-20 10:45:48 -0600

    Fix formatting

--- tests/test_project.py
+++ tests/test_project.py
@@ -5,25 +5,19 @@ import xrdimageutil as xiu
 
 # PROJECT INSTANTIATION
 def test_project_instatiation_with_valid_input_yields_not_null():
-    project = xiu.Project(
-        project_path="data/singh"
-    )
+    project = xiu.Project(project_path="data/singh")
     assert project is not None
 
 def test_project_instatiation_with_nonexisting_path_yields_error():
     try:
-        project = xiu.Project(
-            project_path="./nonexistingpath"
-        )
+        project = xiu.Project(project_path="./nonexistingpath")
         assert False
     except NotADirectoryError:
         assert True
 
 def test_project_instatiation_with_nonstring_path_yields_type_error():
     try:
-        project = xiu.Project(
-            project_path=47
-        )
+        project = xiu.Project(project_path=47)
         assert False
     except TypeError:
         assert True
@@ -32,9 +26,7 @@ def test_project_instatiation_with_empty_path_yields_value_error():
     empty_path = os.path.abspath("empty_path")
     os.mkdir(empty_path)
     try: 
-        project = xiu.Project(
-            project_path=empty_path
-        )
+        project = xiu.Project(project_path=empty_path)
         os.rmdir(empty_path)
         assert False
     except ValueError:
