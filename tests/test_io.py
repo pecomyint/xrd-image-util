17b5cd9c87129ddf83b1319ab7fa975fe008f1c4
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-12-20 08:41:25 -0600

    Add check for missing 'documents' folder.

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,1 +1,25 @@
+import os
+
 from xrdimageutil.io import read_from_databroker
+
+def test_read_from_databroker_valid_input_no_errors():
+    path = os.path.abspath("data/singh")
+    try:
+        source_data = read_from_databroker(path)
+        assert True
+    except:
+        assert False
+
+def test_read_from_databroker_invalid_input_documents_folder_does_not_exist():
+    path = os.path.abspath("data/fake_data")
+    os.mkdir(path)
+    ef_path = os.path.abspath("data/fake_data/external_files")
+    os.mkdir(ef_path)
+    try:
+        source_data = read_from_databroker(path)
+        assert False
+    except ValueError:
+        assert True
+    os.rmdir(ef_path)
+    os.rmdir(path)
+    
--- xrdimageutil/io.py
+++ xrdimageutil/io.py
@@ -2,4 +2,6 @@ from databroker import catalog
 import os
 
 def read_from_databroker(path: str):
-    ...
+    path_items = os.listdir(path)
+    if "documents" not in path_items:
+        raise ValueError(f"'documents' subdirectory not found in path '{path}'.")
