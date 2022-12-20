7e9a77f8c77e1a63c82d07524a4635bbc0520076
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-12-20 10:03:19 -0600

    Add Catalog class skeleton

--- tests/test_project.py
+++ tests/test_project.py
@@ -5,7 +5,7 @@ import xrdimageutil as xiu
 # PROJECT INSTANTIATION
 def test_project_instatiation_with_valid_input_yields_not_null():
     project = xiu.Project(
-        project_path="."
+        project_path="data/singh"
     )
     assert project is not None
 
@@ -40,3 +40,20 @@ def test_project_instatiation_with_empty_path_yields_value_error():
         os.rmdir(empty_path)
         assert True
          
+def test_list_catalogs_with_valid_input():
+    project = xiu.Project(
+        project_path="data/singh"
+    )
+    pr_cat_list = project.list_catalogs()
+
+    assert pr_cat_list == ["henry"]
+
+def test_get_catalog_with_invalid_input_yields_key_error():
+    project = xiu.Project(
+        project_path="data/singh"
+    )
+    try:
+        cat = project.get_catalog(47)
+        assert False
+    except KeyError:
+        assert True
--- xrdimageutil/__init__.py
+++ xrdimageutil/__init__.py
@@ -1,11 +1,17 @@
 import os
 
+from xrdimageutil.io import read_from_databroker
+
 class Project:
-    """Houses data for a collection of scans.
+    """Houses databroker source data and a list of Catalog objects.
     
     :param project_path: A path (relative or absolute) to the project directory
     :type project_path: str
     """
+
+    path = None # Absolute path of project directory
+    data = None # Databroker source data for project
+    catalogs = None # Dict of Catalog objects for project
 
     def __init__(self, project_path: str) -> None:
         
@@ -16,5 +22,53 @@ class Project:
         if len(os.listdir(project_path)) == 0:
             raise ValueError(f"Path '{project_path}' is empty.")
 
-        # Converts path to absolute path
         self.path = os.path.abspath(project_path)
+        self.data = read_from_databroker(project_path)
+
+        if len(list(self.data.keys())) == 0:
+            raise AttributeError(f"Path {self.path} has no catalogs available.")
+
+        # Creates Catalog objects
+        self.catalogs = {}
+        for cat_name in self.data.keys():
+            cat = Catalog(project=self, name=cat_name)
+            self.catalogs.update({cat_name: cat})
+
+    def list_catalogs(self) -> list:
+        """Returns a list of Catalog names for a Project
+        
+        :rtype: list
+        """
+
+        return list(self.data.keys())
+
+    def get_catalog(self, cat_name: str) -> None:
+        """Returns a Catalog from a given name.
+        
+        :param cat_name: Catalog name
+        :type cat_name: str
+
+        :rtype: Catalog
+        """
+        
+        if cat_name not in list(self.data.keys()):
+            raise KeyError(f"Catalog name '{cat_name}' does not exist.")
+
+        return self.catalogs[cat_name]
+
+
+class Catalog:
+    """Houses a collection of scans and metadata.
+    
+    :param project:
+    :type project: xrdimageutil.Project
+    :param name:
+    :type name: str
+    """
+    
+    def __init__(self, project: Project, name: str) -> None:
+        pass
+
+
+
+
