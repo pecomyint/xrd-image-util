6f9e16192ff1900396fcdf7824043719b28ae6fe
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-12-20 08:09:41 -0600

    Add check for empty path

--- xrdimageutil/__init__.py
+++ xrdimageutil/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 class Project:
-    """Houses data for a collection of databroker catalogs.
+    """Houses data for a collection of scans.
     
     :param project_path: A path (relative or absolute) to the project directory
     :type project_path: str
@@ -15,10 +15,9 @@ class Project:
         if type(project_path) != str:
             raise TypeError(f"Path '{project_path}' is an invalid path.")
         if not os.path.exists(project_path):
-            raise ValueError(f"Path '{project_path}' does not exist.")
+            raise NotADirectoryError(f"Path '{project_path}' does not exist.")
+        if len(os.listdir(project_path)) == 0:
+            raise ValueError(f"Path '{project_path}' is empty.")
 
         # Converts path to absolute path
         self.path = os.path.abspath(project_path)
-
-        
-        
