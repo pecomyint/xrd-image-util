0ba4029dcaa6b7d211369842e0babed3413cb9b4
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-12-20 09:02:44 -0600

    Add read_from_databroker function

--- tests/test_io.py
+++ tests/test_io.py
@@ -2,7 +2,7 @@ import os
 
 from xrdimageutil.io import read_from_databroker
 
-def test_read_from_databroker_valid_input_no_errors():
+def test_read_from_databroker_with_valid_input_yields_no_errors():
     path = os.path.abspath("data/singh")
     try:
         source_data = read_from_databroker(path)
@@ -10,16 +10,64 @@ def test_read_from_databroker_valid_input_no_errors():
     except:
         assert False
 
-def test_read_from_databroker_invalid_input_documents_folder_does_not_exist():
+def test_read_from_databroker_with_nonexisting_path_yields_error():
+    try:
+        source_data = read_from_databroker(path="./nonexistingpath")
+        assert False
+    except NotADirectoryError:
+        assert True
+
+def test_read_from_databroker_with_nonstring_path_yields_type_error():
+    try:
+        source_data = read_from_databroker(path=47)
+        assert False
+    except TypeError:
+        assert True
+
+def test_read_from_databroker_with_empty_path_yields_value_error():
+    empty_path = os.path.abspath("empty_path")
+    os.mkdir(empty_path)
+    try: 
+        source_data = read_from_databroker(path=empty_path)
+        os.rmdir(empty_path)
+        assert False
+    except ValueError:
+        os.rmdir(empty_path)
+        assert True
+
+def test_read_from_databroker_with_invalid_input_yields_documents_folder_does_not_exist():
     path = os.path.abspath("data/fake_data")
     os.mkdir(path)
     ef_path = os.path.abspath("data/fake_data/external_files")
     os.mkdir(ef_path)
     try:
         source_data = read_from_databroker(path)
+        os.rmdir(ef_path)
+        os.rmdir(path)
         assert False
     except ValueError:
+        os.rmdir(ef_path)
+        os.rmdir(path)
         assert True
-    os.rmdir(ef_path)
-    os.rmdir(path)
-    
+
+def test_read_from_databroker_with_invalid_input_yields_external_files_folder_does_not_exist():
+    path = os.path.abspath("data/fake_data")
+    os.mkdir(path)
+    d_path = os.path.abspath("data/fake_data/documents")
+    os.mkdir(d_path)
+    try:
+        source_data = read_from_databroker(path)
+        os.rmdir(d_path)
+        os.rmdir(path)
+        assert False
+    except ValueError:
+        os.rmdir(d_path)
+        os.rmdir(path)
+        assert True
+
+def test_read_from_databroker_with_valid_input_yields_1_catalog():
+    path = os.path.abspath("data/singh")
+    source_data = read_from_databroker(path)
+
+    if len(list(source_data.keys())) == 1:
+        assert True
