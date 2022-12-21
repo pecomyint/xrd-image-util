7ba9648dc84e1c5e7509356c61ccc5f00b3f59e9
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2022-12-21 13:04:15 -0600

    Comment out unpacking function

--- xrdimageutil/utils.py
+++ xrdimageutil/utils.py
@@ -1,31 +1,25 @@
+import databroker as db
 import os
-import subprocess
 
 from xrdimageutil.pilatus_handler import PilatusHDF5Handler
 
+'''
+def _unpack_catalog(path: str, name: str) -> None:
+    if name not in list(db.catalog):
+        cat_list = list(db.catalog)
+        while name not in cat_list:
+            try:
+                os.system(f"databroker-unpack inplace {path} {name}")
+            except ValueError:
+                pass
+            cat_list = list(db.catalog)
+    else:
+        raise NameError(f"Catalog '{name}' already exists.")
+ '''       
 
-def _unpack_project(project) -> None:
-    """Unpacks all catalogs in a project."""
-
-    path = project.path
-    cat_list_bytes = subprocess.check_output(
-        f"databroker-unpack --list-catalogs {path}", 
-        shell=True
-    )
-    cat_list_str = cat_list_bytes.decode("utf-8")
-    cat_list_str = cat_list_str.strip("\n")
-    cat_list = cat_list_str.split("\n")
-
-    for cat in cat_list:
-        try:
-            os.system(f"databroker-unpack inplace {path} {cat}")
-        except ValueError:
-            pass
-
-def _prepare_catalog(raw_catalog) -> None:
-    """Registers specific area detector handler."""
-
-    raw_catalog.register_handler(
+def _add_catalog_handler(catalog) -> None:
+    db_catalog = catalog.db_catalog
+    db_catalog.register_handler(
         "AD_HDF5_Pilatus_6idb", 
         PilatusHDF5Handler, 
         overwrite=True
