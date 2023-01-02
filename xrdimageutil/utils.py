370dc90a0580b793ba43cc055d3711fa9108ce65
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2023-01-02 12:03:43 -0600

    Move pilatus handler to utils

--- xrdimageutil/utils.py
+++ xrdimageutil/utils.py
@@ -1,4 +1,20 @@
-from xrdimageutil.pilatus_handler import PilatusHDF5Handler
+import area_detector_handlers.handlers as adh
+from dask.array import from_array
+
+
+class PilatusHDF5Handler(adh.AreaDetectorHDF5SingleHandler):
+    """Handler for the Pilatus detector HDF5 files. This version is
+    geared specifically towards beamline 6-ID-B.
+
+    Originally from Gilberto Fabbris.
+    """
+
+    specs = (
+        {"AD_HDF5_Pilatus_6idb"} | adh.AreaDetectorHDF5SingleHandler.specs
+    )
+
+    def __call__(self, point_number):
+        return from_array(super().__call__(point_number))
 
 
 def _add_catalog_handler(catalog) -> None:
