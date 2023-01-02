3365cb5a24b1c9972694b4d642766fe7441500f7
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2023-01-02 09:48:14 -0600

    Add kwargs

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -1,19 +1,29 @@
 import os
 
-from xrdimageutil import Catalog
+from xrdimageutil import Catalog, Scan
 
-class TestInstantiation:
+class TestCatalog:
 
     relative_path = "data/singh"
-    absolute_path = os.path.abspath(relative_path)
+    absolute_path = os.path.abspath(path=relative_path)
     catalog_name = "test-catalog"
 
     def test_instantiation_with_valid_name_expects_not_none_type(self):
-        catalog = Catalog(self.catalog_name)
+        catalog = Catalog(name=self.catalog_name)
         assert type(catalog) is not None
 
     def test_instantiation_with_invalid_name_expects_key_error(self):
         try:
-            catalog = Catalog("invalid-name")
+            catalog = Catalog(name="invalid-name")
         except KeyError:
             assert True
+
+    def test_list_scans_for_count_expects_19(self):
+        catalog = Catalog(name=self.catalog_name)
+        assert len(catalog.list_scans()) == 19
+
+    def test_get_scan_for_return_type_expects_not_none(self):
+        catalog = Catalog(name=self.catalog_name)
+        scan_id = catalog.list_scans()[0]
+        scan = catalog.get_scan(scan_id=scan_id)
+        assert type(scan) is not None
