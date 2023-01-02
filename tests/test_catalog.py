4b056ac5174c05d4553671f7da528d64084d84b1
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2023-01-02 09:39:35 -0600

    Add invalid key test

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -6,9 +6,14 @@ class TestInstantiation:
 
     relative_path = "data/singh"
     absolute_path = os.path.abspath(relative_path)
-    catalog_key = "test-catalog"
+    catalog_name = "test-catalog"
 
-    def test_instantiation_with_valid_key_expects_not_none_type(self):
-        catalog = Catalog(self.catalog_key)
+    def test_instantiation_with_valid_name_expects_not_none_type(self):
+        catalog = Catalog(self.catalog_name)
         assert type(catalog) is not None
 
+    def test_instantiation_with_invalid_name_expects_key_error(self):
+        try:
+            catalog = Catalog("invalid-name")
+        except KeyError:
+            assert True
