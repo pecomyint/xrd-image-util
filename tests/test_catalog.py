5e78e4c0e093ff253b4b66651c5e0c570bf47528
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2023-01-02 09:10:35 -0600

    Remove in-class unpacking -> moved to tox

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -8,10 +8,6 @@ class TestInstantiation:
     absolute_path = os.path.abspath(relative_path)
     catalog_key = "test-catalog"
 
-    @classmethod
-    def setup_class(self):
-        os.system(f"databroker-unpack inplace {self.relative_path} {self.catalog_key}")
-
     def test_instantiation_with_valid_key_expects_not_none_type(self):
         catalog = Catalog(self.catalog_key)
         assert type(catalog) is not None
