97796ef12e4fcb92d6e5f72cdc44667a02a7ae1e
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2023-01-02 09:50:57 -0600

    Remove functions out of v0.0.1 scope (unpacking)

--- xrdimageutil/utils.py
+++ xrdimageutil/utils.py
@@ -1,18 +1,5 @@
 from xrdimageutil.pilatus_handler import PilatusHDF5Handler
 
-'''
-def _unpack_catalog(path: str, name: str) -> None:
-    if name not in list(db.catalog):
-        cat_list = list(db.catalog)
-        while name not in cat_list:
-            try:
-                os.system(f"databroker-unpack inplace {path} {name}")
-            except ValueError:
-                pass
-            cat_list = list(db.catalog)
-    else:
-        raise NameError(f"Catalog '{name}' already exists.")
- '''       
 
 def _add_catalog_handler(catalog) -> None:
     db_catalog = catalog.db_catalog
