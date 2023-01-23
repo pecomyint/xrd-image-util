2d4074c3b69732fb4a41c421302b4398d783a6fb
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2023-01-23 09:51:01 -0600

    Add coordinates to axes

--- xrdimageutil/gui.py
+++ xrdimageutil/gui.py
@@ -3,7 +3,7 @@ See LICENSE file.
 """
 
 import numpy as np
-from PyQt5 import QtWidgets
+from PyQt5 import QtGui, QtWidgets
 import pyqtgraph as pg
 from pyqtgraph.dockarea import Dock, DockArea
 
@@ -74,7 +74,6 @@ class RawDataWidget(DockArea):
             max=np.amax(scan.raw_data)
         )
         self.image_widget.setColorMap(colormap=self.colormap)
-        self.image_widget.ui.roiPlot.getAxis("bottom").setLabel("t")
 
         # DockArea setup
         self.image_dock = Dock(
@@ -91,7 +90,7 @@ class GriddedDataWidget(DockArea):
         super(GriddedDataWidget, self).__init__()
 
         self.scan = scan
-
+        
         # Image widget setup
         self.image_widget = pg.ImageView( 
             view=pg.PlotItem()
@@ -101,16 +100,29 @@ class GriddedDataWidget(DockArea):
         self.image_widget.ui.menuBtn.hide()
         self.image_widget.getView().setAspectLocked(False)
         self.image_widget.getView().ctrlMenu = None
-        self.image_widget.setImage(scan.gridded_data)
         self.colormap = utils._create_colormap(
             name="turbo",
             scale="log",
             max=np.amax(scan.gridded_data)
         )
         self.image_widget.setColorMap(colormap=self.colormap)
-        self.image_widget.ui.roiPlot.getAxis("bottom").setLabel("H")
         self.image_widget.getView().setLabel("bottom", "K")
         self.image_widget.getView().setLabel("left", "L")
+        self.transform = QtGui.QTransform()
+        gdc = scan.gridded_data_coords
+        scale = (
+            gdc[1][1] - gdc[1][0],
+            gdc[2][1] - gdc[2][0]
+        )
+        pos = [gdc[1][0], gdc[2][0]]
+        self.transform.translate(*pos)
+        self.transform.scale(*scale)
+        self.image_widget.setImage(
+            img=scan.gridded_data,
+            transform=self.transform,
+            xvals=gdc[0]
+        )
+        self.image_widget.setCurrentIndex(0)
 
         # DockArea setup
         self.image_dock = Dock(
