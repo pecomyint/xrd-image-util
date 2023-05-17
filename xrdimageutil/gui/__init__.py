311d531ae21ac3e57e96658043d5de404b76e757
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2023-05-17 11:57:50 -0500

    Add input validation for general ImageGUI entry point

--- xrdimageutil/gui/__init__.py
+++ xrdimageutil/gui/__init__.py
@@ -4,13 +4,35 @@ import pyqtgraph as pg
 from xrdimageutil.gui import image_data_widget
 
 def view_image_data(data: np.ndarray, coords: dict=None) -> None:
+    """Displays 3D numpy data in an interactive GUI window."""
     
+    # Data validation
+    if type(data) != np.ndarray:
+        raise TypeError("'data' must be of the type np.ndarray.")
+    
+    if data.ndim != 3:
+        raise ValueError("'data' must be an np.ndarray with ndim=3.")
+
+    # Coords validation
     if coords is None:
         coords = {
             "x": np.linspace(0, data.shape[0], data.shape[0] - 1),
             "y": np.linspace(0, data.shape[1], data.shape[1] - 1),
             "z": np.linspace(0, data.shape[2], data.shape[2] - 1)
         }
+    else:
+        if type(coords) != dict:
+            raise TypeError("'coords' must be a dictionary.")
+        if len(list(coords.keys)) != 3:
+            raise ValueError("'coords' must have keys for all three dimensions.")
+        for dim in list(coords.keys):
+            if coords[dim] is None:
+                coords[dim] == (None, None)
+            if type(coords[dim]) != list and type(coords[dim]) != tuple:
+                raise ValueError("Invalid 'coords' provided.")
+            if len(coords[dim]) != 2:
+                raise ValueError("Invalid 'coords' provided.")
+            
 
     app = pg.mkQApp()
     gui_window = image_data_widget.ImageDataWidget(data, coords)
