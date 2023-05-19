571cc27b74e9c878502d09191350d4a11c0342f2
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2023-05-19 09:27:19 -0500

    Add error handling for RectROI

--- xrdimageutil/roi.py
+++ xrdimageutil/roi.py
@@ -1,23 +1,30 @@
 import numpy as np
-from skimage.draw import line_nd
-
-import xrdimageutil as xiu
-from xrdimageutil import utils
-
 
 class RectROI:
+    """A rectangular region of interest that can be applied to 3D dataset.
+    
+    
+    """
 
-    data_type = None
     bounds = None
-    output_type = None
+    calculation = None
     output = None
     
-    def __init__(self, dims) -> None:
+    def __init__(self, dims: list=None) -> None:
 
-        self.bounds = dict((dim, (None, None)) for dim in dims)
+        if dims is None:
+            self.bounds = {
+                "x": (None, None),
+                "y": (None, None),
+                "z": (None, None)
+            }
+        else:
+            if len(dims) != 3:
+                raise ValueError("Invalid dims provided.")
+            self.bounds = dict((dim, (None, None)) for dim in dims)
         
-        self.output_type = {
-            "output": None,
+        self.calculation = {
+            "output_data": None,
             "dims": None
         }
 
@@ -29,9 +36,30 @@ class RectROI:
     def set_bounds(self, bounds: dict) -> None:
         """Sets coordinate bounds for the RectROI."""
         
-        self.bounds = {dim: bounds[dim] for dim in list(bounds.keys())}
+        if type(bounds) != dict:
+            raise ValueError("Invalid bounds provided.")
+        if len(list(bounds.keys())) != 3:
+            raise ValueError("Invalid bounds provided.")
+        for dim in list(bounds.keys()):
+            dim_bounds = bounds[dim]
+            if type(dim_bounds) is None:
+                bounds[dim] == (None, None)
+            if type(dim_bounds) != list and type(dim_bounds) != tuple:
+                raise ValueError("Invalid bounds provided.")
+            
+            if len(dim_bounds) != 2:
+                raise ValueError("Invalid bounds provided.")
+            if None not in bounds[dim] and dim_bounds[1] < dim_bounds[0]:
+                raise ValueError("Invalid bounds provided.")
 
-    def set_output_type(self, output: str, dims: list) -> None:
+        if set(list(bounds.keys())) == set(list(self.bounds.keys())):
+            self.bounds = {dim: bounds[dim] for dim in list(self.bounds.keys())}
+        else:
+            self.bounds = {dim: bounds[dim] for dim in list(bounds.keys())}
+
+    def set_calculation(self, output: str, dims: list) -> None:
+        """Sets the output calculation (average, max) and the dimensions to calculate on."""
+
         if dims is not None:
             if not set(list(self.bounds.keys())).issuperset(set(dims)):
                 raise ValueError("Invalid dimension list provided.")
@@ -39,20 +67,21 @@ class RectROI:
         if output not in ["average", "max"]:
             raise ValueError("Invalid output type provided. Accepted values are 'average' and 'max'.")
         
-        self.output_type = {
+        self.calculation = {
             "output": output,
             "dims": dims
         }
-
+    
     def apply(self, data, coords) -> None:
-        
-        output_dims = self.output_type["dims"]
-        output_type = self.output_type["output"]
+        """Carries out a calculation (see the 'output_type' attribute) on a dataset."""
+
+        output_dims = self.calculation["dims"]
+        output_type = self.calculation["output"]
         
         if output_dims is None:
             output_dims = []
         if output_type is None:
-            raise ValueError("No output type found. Please add a output type using 'set_output_type'.")
+            raise ValueError("No output type found. Please add a output type using 'set_calculation'.")
 
         coords = coords.copy()
 
@@ -136,5 +165,20 @@ class RectROI:
             else:
                 raise ValueError("Invalid dimension list.")
 
+    def apply_to_scan(self, scan, data_type) -> None:
+        
+        if data_type == "raw":
+            data = scan.raw_data["data"]
+            coords = scan.raw_data["coords"]
+        elif data_type == "gridded":
+            data = scan.gridded_data["data"]
+            coords = scan.gridded_data["coords"]
+        else:
+            raise("Invalid data type provided.")
+        
+        self.apply(data, coords)
+    
     def get_output(self) -> dict:
+        """Returns the output from the most recent apply() run."""
+        
         return self.output
