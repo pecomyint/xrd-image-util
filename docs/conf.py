aa554abff300d1e6d04365b9616af6b9b0ce7f97
henryjsmith12 <48372336+henryjsmith12@users.noreply.github.com>
2023-05-22 08:47:33 -0500

    Autodoc attempt

--- docs/conf.py
+++ docs/conf.py
@@ -3,10 +3,12 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
 import pathlib
 import sys
+sys.path.insert(0, os.path.abspath("../"))
+#sys.path.append(str(pathlib.Path(__file__).parent.parent.parent))
 
-sys.path.append(str(pathlib.Path(__file__).parent.parent.parent))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
